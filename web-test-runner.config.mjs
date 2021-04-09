de671f2a18eb27fc51b320d7afea3a59bdc65469
Pawel Psztyc <jarrodek@gmail.com>
2021-04-09 10:45:24 -0700

    test: increaing browser timeout
    Signed-off-by: Pawel Psztyc <jarrodek@gmail.com>

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -1,8 +1,11 @@
-export default {
+/** @typedef {import('@web/test-runner').TestRunnerConfig} TestRunnerConfig */
+
+export default /** @type TestRunnerConfig */ ({
   // concurrency: 1,
   testFramework: {
     config: {
       timeout: 10000,
     },
   },
-}
+  testsFinishTimeout: 180000,
+})
